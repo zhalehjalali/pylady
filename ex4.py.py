2fa0ac33d330d0b9b2d77459518f00d9e05557dd
zhaleh j <zhaleh_j1986@yahoo.com>
2014-12-10 21:45:11 +0100

    my new file

---
+++ ex4.py.py
@@ -0,0 +1,26 @@
+print "I will count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Rooster", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7 
+
+print "what is 3 + 2?", 3 +  2
+print "what is 5 - 7?", 5 - 7
+ 
+print "oh, that's why it's False."
+
+print "how about some more"
+
+print "Is it greater?", 5 > -2
+print "is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+ 
+ print "hi"
+ 
